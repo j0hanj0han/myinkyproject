2f3ea2be6d57767f62b41a9d3ca9f15c9be65363
Johan <johan.chapelain@gmail.com>
2020-04-05 19:33:35 +0200

    first draw image

--- myinky.py
+++ myinky.py
@@ -1,21 +1,23 @@
 from PIL import Image, ImageFont, ImageDraw
+from font_hanken_grotesk import HankenGroteskBold, HankenGroteskMedium
 
 # new drawing object
-screen_width = ""
-screen_height =""
+screen_width = 212
+screen_height = 104
+padding = 0
+scale_size = 1
 img = Image.new("P", (screen_width, screen_height))
 draw = ImageDraw.Draw(img)
 
 # message to show
-text = "Hello"
-text_w, text_h = hanken_bold_font.getsize(text)
+hanken_bold_font = ImageFont.truetype(HankenGroteskBold, int(35 * scale_size))
 
-hello_x = int((inky_display.WIDTH - hello_w) / 2)
+# Calculate the positioning and draw the "Hello" text
+
+hello_w, hello_h = hanken_bold_font.getsize("Hello")
+hello_x = int((screen_width - hello_w) / 2)
 hello_y = 0 + padding
-
-#draw
-draw.text((text_x, text_y), text, inky_display.WHITE, font=hanken_bold_font)
-
+draw.text((hello_x, hello_y),"white", "Hello") 
 
 #save the image 
 img.save("test.png", "PNG")
