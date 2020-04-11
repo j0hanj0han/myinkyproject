bcfafcd5fb0b8c429dc95fbf07206b3310f53e13
Johan <johan.chapelain@gmail.com>
2020-04-11 14:41:02 +0200

    flake8 reformatting

--- myinky.py
+++ myinky.py
@@ -3,6 +3,7 @@ from font_hanken_grotesk import HankenGroteskBold, HankenGroteskMedium
 import datetime
 import weatherstack
 import requests
+
 # from inky import InkyMockPHAT as InkyPHAT
 # colour = "black"
 # inky_display = InkyPHAT(colour)
@@ -11,8 +12,9 @@ import requests
 screen_width = 212
 screen_height = 104
 img = Image.new("P", (screen_width, screen_height))
-#img = Image.new("P", (inky_display.WIDTH, inky_display.HEIGHT))
+# img = Image.new("P", (inky_display.WIDTH, inky_display.HEIGHT))
 draw = ImageDraw.Draw(img)
+
 
 def write_text(text, padding):
     text = text
@@ -20,7 +22,15 @@ def write_text(text, padding):
     text_w, text_h = font.getsize(text)
     text_x = int((screen_width - text_w) / 2)
     text_y = padding
-    draw.text(xy=(text_x, text_y),text=text, stroke_fill="black",fill="white", stroke_width=10, font=font ) 
+    draw.text(
+        xy=(text_x, text_y),
+        text=text,
+        stroke_fill="black",
+        fill="white",
+        stroke_width=10,
+        font=font,
+    )
+
 
 def draw_lines():
     # draw a border around the screen
@@ -29,13 +39,13 @@ def draw_lines():
     x_top = int(screen_width)
 
     for y in range(0, y_top):
-        img.putpixel(xy=(0, y), value=(0,0,0))
-        #split in two frame
-        #img.putpixel(xy=(103, y), value=(0,0,0))
-        img.putpixel(xy=(211, y), value=(0,0,0))
+        img.putpixel(xy=(0, y), value=(0, 0, 0))
+        # split in two frame
+        # img.putpixel(xy=(103, y), value=(0,0,0))
+        img.putpixel(xy=(211, y), value=(0, 0, 0))
     for x in range(0, x_top):
-        img.putpixel(xy=(x, 0), value=(0,0,0))
-        img.putpixel(xy=(x, 103), value=(0,0,0))
+        img.putpixel(xy=(x, 0), value=(0, 0, 0))
+        img.putpixel(xy=(x, 103), value=(0, 0, 0))
 
 
 # message to show
@@ -43,16 +53,18 @@ def add_text(text_to_add=None):
     text = "Hello Johan !"
     write_text(text, 5)
 
+
 def add_time():
     now = datetime.datetime.now()
     now = now.strftime("%Y-%m-%d %H:%M:%S")
     write_text(now, 20)
 
+
 def add_meteo():
     meteo = weatherstack.get_weather()
     meteo = meteo[0]
     write_text(meteo, 40)
-    
+
 
 # # Display the completed name badge
 # inky_display.set_image(img)
@@ -60,7 +72,7 @@ def add_meteo():
 
 
 if __name__ == "__main__":
-    
+
     add_text()
     add_time()
     add_meteo()
--- weatherstack.py
+++ weatherstack.py
@@ -3,43 +3,45 @@ import pprint
 import json
 
 pp = pprint.PrettyPrinter(indent=4)
-city="Montpellier"
-api_key="316cb339b0ae48818f692bfb98c4f727"
+city = "Montpellier"
+api_key = "316cb339b0ae48818f692bfb98c4f727"
 
 
 def connect_to_api():
     try:
-        response = requests.get(f"http://api.weatherstack.com/forecast?access_key={api_key}&query={city}")
+        response = requests.get(
+            f"http://api.weatherstack.com/forecast?access_key={api_key}&query={city}"
+        )
         json_data = json.loads(response.text)
         print(json)
         return json_data
-    except Exception as e: 
+    except Exception as e:
         print(e)
+
+
 # json_data = json.loads(response.text)
 # print(pp.pprint(json_data))
 
 
 def get_weather():
     json_data = connect_to_api()
-    city_name=json_data['request']['query']
-    weather_text=json_data['current']['weather_descriptions'][0]
-    temperature_now=json_data['current']['temperature']
-    date_forecast = str(list(json_data['forecast'].keys())[0])
-    temperature_min=json_data['forecast'][date_forecast]['mintemp']
-    temperature_max=json_data['forecast'][date_forecast]['maxtemp']
+    city_name = json_data["request"]["query"]
+    weather_text = json_data["current"]["weather_descriptions"][0]
+    temperature_now = json_data["current"]["temperature"]
+    date_forecast = str(list(json_data["forecast"].keys())[0])
+    temperature_min = json_data["forecast"][date_forecast]["mintemp"]
+    temperature_max = json_data["forecast"][date_forecast]["maxtemp"]
 
-
-
-    print('------------------')
-    print('Météo pour la ville de :', city_name)
-    print('Temps actuel:', weather_text)
-    print('Température actuelle:', temperature_now)
-    print('Temperature minimale:', temperature_min)
-    print('Temperature maximale:', temperature_max)
-    print('-------------------')
-    meteo=[city_name, weather_text, temperature_now, temperature_min, temperature_max]
+    print("------------------")
+    print("Météo pour la ville de :", city_name)
+    print("Temps actuel:", weather_text)
+    print("Température actuelle:", temperature_now)
+    print("Temperature minimale:", temperature_min)
+    print("Temperature maximale:", temperature_max)
+    print("-------------------")
+    meteo = [city_name, weather_text, temperature_now, temperature_min, temperature_max]
     return meteo
-    
+
+
 # if  __name__ == "__main__":
 #     get_weather()
-    
