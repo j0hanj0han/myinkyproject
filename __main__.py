76dc0dec395d8c71145cf51a5724100c0d90075e
Johan <johan.chapelain@gmail.com>
2020-04-17 17:36:07 +0200

    refacto

--- __main__.py
+++ __main__.py
@@ -1,49 +1,7 @@
-import myinkyproject.binance
-import myinkyproject.inky
+from myinkyproject.binance import binance
+from myinkyproject.inky import inky
 
 if __name__ == "__main__":
+    binance.get_last_price('BTCUSDT')
+    inky.write_text()
     pass
-
-
-
-from PIL import Image, ImageFont, ImageDraw
-import myinkylib
-import datetime
-import requests
-import time
-import prices
-
-def add_prices(pairs_list):
-    img = Image.new("P", (212, 104))
-    draw = ImageDraw.Draw(img)
-
-    prices_list = prices.display_price(pairs_list)
-    padding = 5 
-    for price in prices_list:
-        myinkylib.write_text(price, padding, draw)
-        padding +=10
-    
-    myinkylib.draw_lines(img)
-    img.save("binance_prices.png", "PNG")
-
-if __name__ == "__main__":
-    my_list=['BTCUSDT', 'ETHUSDT', 'LTCUSDT', 'LINKUSDT']
-    add_prices(my_list)
-
-if __name__ == "__main__":
-    my_list=['BTCUSDT', 'ETHUSDT', 'LTCUSDT', 'LINKUSDT']
-    while True:
-        add_prices(my_list)
-        time.sleep(10)
-
-
-import binance_calls as bc 
-
-def display_price(pairs_list):
-    '''return pair and symbol'''
-    result = []
-    for pair in pairs_list:
-        price = round(float(bc.get_last_price(pair)),2)
-        price = f"{pair}: {price} USDT"
-        result.append(price)
-    return result
---
+++ setup.py
@@ -0,0 +1,13 @@
+  
+from setuptools import setup, find_packages
+
+setup(
+    name='myinkyproject',
+    version='1.0.0',
+    url='N/A',
+    author='Johan',
+    author_email='author@gmail.com',
+    description='Description of my package',
+    packages=find_packages(),    
+    install_requires=[],
+)
